1b5c54717c815798b5d2ce3f8e61f1c037d8ac45
Alissa <alissacrane123@gmail.com>
2020-10-28 07:49:56 -0700

    update js files

--- lecture-notes/week-4/d3-event-handling/common-events-demo/index.js
+++ lecture-notes/week-4/d3-event-handling/common-events-demo/index.js
@@ -3,25 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	// 1. update click count via span
 	
 
-	// method #1
-	// let button = document.getElementById("my-button");
-	// let span = document.getElementById("my-span");
-
-	// button.addEventListener("click", (event) => {
-	// 	span.innerText = `${event.detail}`;
-	// });
-
-
-	// method #2
-	let button = document.getElementById("my-button");
-	let span = document.getElementById("my-span");
-	let count = 0;
-	button.addEventListener("click", (event) => {
-		count++;
-		span.innerText = count;
-		// span.innerHTML = '<div>Hi</div>'
-	});
-
+	
 
 
 
@@ -33,21 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // append the image "dog-pic.jpg" to the div with the id of "dog-img"
   // when the user clicks the button with the id of "show-dog-button"
 
-  let showDogButton = document.getElementById("show-dog-button");
-  let dogDiv = document.getElementById("dog-img");
-
-  showDogButton.addEventListener("click", (event) => {
-		// let img = '<img src="./dog-pic.jpg" >'
-		// dogDiv.innerHTML = img;
-
-		let img = document.createElement('img');
-		img.src = './dog-pic.jpg';
-
-		if (dogDiv.innerHTML === '') {
-			dogDiv.appendChild(img);
-		}
-
-	});
+  
 	
 
 
@@ -58,26 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// 3. handling user input
 
-  let userInput = document.getElementById("user-input");
-  let myDiv = document.getElementById("my-div");
-
-  let validColors = [
-    "red",
-    "green",
-    "blue",
-    "yellow",
-    "orange",
-    "pink",
-    "purple",
-  ];
-
-  userInput.addEventListener("input", (event) => {
-		let value = event.target.value;
-		
-		if (validColors.includes(value)) {
-			myDiv.style.backgroundColor = value;
-		}
-  });
+  
 
 
 
