41bf263ae7a9af4052d0c0fb07c81d5d37197172
Alissa <alissacrane123@gmail.com>
2020-10-29 14:25:38 -0700

    create solution for d4 warmup

---
+++ practices/week-4/d4-warmup-event-handling/solution.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', event => {
+
+	let form = document.getElementById('todo-form');
+	let input = document.getElementById('user-input');
+	let list = document.getElementById('todo-list')
+
+	form.addEventListener('submit', event => {
+		event.preventDefault();
+		let li = document.createElement('li')
+		li.innerText = input.value;
+		list.appendChild(li);
+		input.value = '';
+	})
+})
