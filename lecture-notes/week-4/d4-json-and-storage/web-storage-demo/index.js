f1eb6ff3a26e43e871b899d167c53de1b6ecfc55
Alissa <alissacrane123@gmail.com>
2020-10-29 09:27:01 -0700

    add web storage demo

--- lecture-notes/week-4/d4-json-and-storage/web-storage-demo/index.js
+++ lecture-notes/week-4/d4-json-and-storage/web-storage-demo/index.js
@@ -1,34 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
-  // // storing items in local storage
+  // // // storing items in local storage
 	// let key = "TAs";
 	// let value = ["James", "Brad", "Kristen", "Senyo"];
-	// // must serialize value before storing in local storage
-	// // let serializedValue = JSON.stringify(value);
-	// localStorage.setItem(key, value);
+	// // // must serialize value before storing in local storage
+	// let serializedValue = JSON.stringify(value);
+	// localStorage.setItem(key, serializedValue);
 	
 
 
-  // // retrieving items from local storage
+  // // // retrieving items from local storage
 	// let instructors = localStorage.getItem(key);
   // console.log("instructors : ", instructors);
   // console.log("typeof instructors: ", typeof instructors); // string
 
 
-	// // // deserializing values retrieved from local storage
-  // // let deserializedInstructors = JSON.parse(instructors);
-  // // console.log("deserializedInstructors: ", deserializedInstructors); 
-  // // console.log("typeof deserializedInstructors: ", typeof deserializedInstructors); // object
+	// // // // deserializing values retrieved from local storage
+  // let deserializedInstructors = JSON.parse(instructors);
+  // console.log("deserializedInstructors: ", deserializedInstructors); 
+  // console.log("typeof deserializedInstructors: ", typeof deserializedInstructors); // object
 
 
 
-  // // this wont work because instructors is JSON str not a JS obj
-  // // instructors.forEach((ta) => console.log(ta));
+  // // // this wont work because instructors is JSON str not a JS obj
+  // // // instructors.forEach((ta) => console.log(ta));
 
 	
-  // // this will work because deserializedInstructors is now a JS obj
-  // // deserializedInstructors.forEach((ta) => console.log(ta));
+  // // // this will work because deserializedInstructors is now a JS obj
+  // deserializedInstructors.forEach((ta) => console.log(ta));
 
 
 
@@ -49,5 +49,36 @@ document.addEventListener('DOMContentLoaded', () => {
 	// 2. if values exist in local storage, pre populate relevant inputs
 	// with those values
 
-	
+	const firstName = document.getElementById('first');
+	const lastName = document.getElementById('last');
+	const email = document.getElementById('email');
+
+	const firstNameValue = localStorage.getItem('first');
+	const lastNameValue = localStorage.getItem('last');
+	const emailValue = localStorage.getItem('email');
+
+	firstName.value = firstNameValue;
+	lastName.value = lastNameValue;
+	email.value = emailValue;
+
+	const form = document.getElementById('my-form');
+
+	form.addEventListener('submit', event => {
+		event.preventDefault();
+
+		localStorage.setItem('first', firstName.value);
+		localStorage.setItem('last', lastName.value);
+		localStorage.setItem('email', email.value);
+	})
+
+
+	// const inputs = document.querySelectorAll('input');
+
+	// form.addEventListener('submit', event => {
+	// 	event.preventDefault();
+
+	// 	inputs.forEach(input => {
+	// 		localStorage.setItem(input.id, input.value);
+	// 	})
+	// })
 })
