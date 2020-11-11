fd388dd0cb64c47d319be33986b91d047e05fb73
Alissa <alissacrane123@gmail.com>
2020-11-10 16:54:43 -0800

    add star wars async and await

---
+++ week-6/d3-async-await/lecture-demos/star-wars.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch');
+const fs = require('fs').promises;
+
+
+
+let baseUrl = 'https://swapi.dev/api/people/'
+
+
+// TODO: lets rewrite the star wars project using async and await
