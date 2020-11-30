cf955e2e3af9fdf83404a1a3b559f4001172e163
Alissa <alissacrane123@gmail.com>
2020-11-30 07:54:26 -0800

    update warmup

--- week-4/d4-json-and-storage/warmup-event-handling/your-code.js
+++ week-4/d4-json-and-storage/warmup-event-handling/your-code.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', event => {
 
+	// TODO: 
+	// when the FORM with id 'todo-form' is submitted:
+	// - create a new LI element
+	// - set the inner text of the newly created LI to the value the user typed
+	//   into the INPUT with id 'user-input'
+	// - append the LI to the UL with id 'todo-list'
+	// - reset the value of the INPUT to be an empty string
+	
+
+
 	
 })
