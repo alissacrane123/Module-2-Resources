33a66cc1cc8b21a76a4bd2dff54e4a2fb720f993
Alissa <alissacrane123@gmail.com>
2020-11-30 14:30:44 -0800

    add warmup to w5d2 classes

---
+++ week-5/d2-classes/warmup/solution.js
@@ -0,0 +1,73 @@
+
+class User {
+	constructor(name) {
+		this.name = name;
+		this.account = null;
+	}
+
+	signup(newAccount) {
+		this.account = newAccount;
+		console.log(`${this.name} signed up for a library account`)
+	}
+
+	rentBook(book) {
+		this.account.receiveBook(book)
+		console.log(`${this.name} received a new book, ${book}`);
+	}
+}
+
+
+class Account {
+	constructor(userName) {
+		this.userName = userName;
+		this.books = [];
+	}
+
+	receiveBook(book) {
+		this.books.push(book);
+	}
+}
+
+
+class Library {
+	constructor(books) {
+		this.books = books;
+	}
+
+	signupUser(user) {
+		if (user.account) {
+			console.log(`${user.name} already has an account`)
+		} else {
+			let newAccount = new Account(user.name)
+			user.signup(newAccount)
+		}
+	}
+
+	removeBook(bookTitle) {
+		this.books = this.books.filter(book => {
+			return book !== bookTitle;
+		});
+	}
+
+	loanBook(user, bookTitle) {
+		if (!this.books.includes(bookTitle)) {
+			console.log(`sorry we dont have the book "${bookTitle}"`);
+		} else if (!user.account) {
+			console.log(`sorry, ${user.name} does not have an account`);
+		} else {
+			user.rentBook(bookTitle);
+			this.removeBook(bookTitle)	
+		}
+	}
+}
+
+
+let books = ['harry potter', 'to kill a mockingbird'];
+let user = new User('alissa');
+let library = new Library(books);
+
+library.signupUser(user); // alissa signed up for a library account
+library.loanBook(user, 'to kill a mockingbird') // alissa received a new book, to kill a mockingbird
+library.loanBook(user, 'harry potter'); // alissa received a new book, harry potter
+library.loanBook(user, 'the kite runner'); // sorry we dont have the book the kite runner
+library.signupUser(user); // alissa already has an account
