a5d17f166918261dad8bfa54463ff50076302f0a
Alissa <alissacrane123@gmail.com>
2020-12-02 08:45:34 -0800

    update lecture notes d2

--- week-5/d2-classes/lecture-notes/lecture-demos/inheritance.js
+++ week-5/d2-classes/lecture-notes/lecture-demos/inheritance.js
@@ -1,53 +1,22 @@
 // INHERITANCE
 
 
-// * TODO = CREATE PARENT CLASS FOR DOGS AND CATS
+// * TODO = CREATE PARENT CLASS FOR DOGS AND CATS CALLED ANIMAL
 // - dogs and cats both have a name and age property
 // - dogs and cats both have a speak method
 
-// class Animal {
-// 	constructor(name, age) {
-// 		this.name = name;
-// 		this.age = age;
-// 	}
-
-// 	speak() {
-// 		console.log(`hi my name is ${this.name}`)
-// 	}
-// }
 
 
-// class Dog extends Animal {
-// 	constructor(name, age) {
-// 		super(name, age);
-// 		this.tricks = [];
-// 	}
 
-// 	bark() {
-// 		console.log('bark bark bark')
-// 	}
-// }
 
-// class Cat extends Animal {
-// 	// constructor(name, age) {
-// 	// 	super(name, age);
-// 	// }
 
-// 	meow() {
-// 		console.log('meow meow meow')
-// 	}
-// }
 
-// let bodhi = new Dog('bodhi', 5);
-// bodhi.speak(); // hi my name is bodhi
-// console.log(bodhi) // Dog { name: 'bodhi', age: 5, tricks: [] }
 
-// let pancake =  new Cat('pancake', 10);
-// pancake.meow();
-// pancake.speak();
 
-// console.log(Dog);
-// console.log(Animal);
+
+
+
+
 
 
 
@@ -117,27 +86,31 @@
 
 
 
+
+
+
+
 // CAN OVERWRITE AND ADD ONTO INHERITED METHODS
 
-class Animal {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+// class Animal {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
 
-  speak(word) {
-    console.log(`hi my name is ${this.name}, ${word}`);
-  }
-}
+//   speak(word) {
+//     console.log(`hi my name is ${this.name}, ${word}`);
+//   }
+// }
 
 // TODO : OVERWRITE SPEAK METHOD TO ALSO PRINT "bark bark bark"
 
-class Dog extends Animal {
-	speak() {
-		super.speak('wah');
-		console.log('bark bark bark');
-	}
-}
+// class Dog extends Animal {
+// 	speak() {
+// 		super.speak('HELLO');
+// 		console.log('bark bark bark');
+// 	}
+// }
 
-let bodhi = new Dog('bodhi', 5);
-bodhi.speak();
+// let bodhi = new Dog('bodhi', 5);
+// bodhi.speak();
