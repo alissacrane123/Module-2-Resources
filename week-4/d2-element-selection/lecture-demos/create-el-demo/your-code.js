79b553af35dfd876e773e33d729d3faa2e227cc6
Alissa <alissacrane123@gmail.com>
2021-01-05 11:00:04 -0800

    update d2-element-selection lecture code

--- week-4/d2-element-selection/lecture-demos/create-el-demo/your-code.js
+++ week-4/d2-element-selection/lecture-demos/create-el-demo/your-code.js
@@ -2,7 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// * TODO = ADD H1 TO HEADER
 
-	
+	// method #1
+	let header = document.getElementById('my-header');
+	header.innerHTML = '<h1>Hello World</h1>';
+
+	// method #2
+	// let h1 = document.createElement('h1');
+	// // console.log(h1)
+	// h1.innerText = 'hello world';
+	// // console.log(h1);
+	// header.appendChild(h1);
+
+
+	// method #3
+	// let h1 = document.createElement('h1');
+	// let text = document.createTextNode('hello world');
+	// h1.appendChild(text);
+	// header.appendChild(h1);
+
 	
 
 	// * TODO = ADD ITEMS TO COLOR AND NUMBER LISTS WITH CLASS NAME
@@ -13,9 +30,46 @@ document.addEventListener('DOMContentLoaded', () => {
 	// example className = 'color__item';
 
 	function addItems(items, parent, className) {
-		
+		items.forEach(item => {
+			let li = document.createElement('li');
+			// <li></li>
+			li.setAttribute('class', className);
+			// <li class="color__item"></li>
+			li.innerText = item;
+			// <li class="color__item">Red</li>
+			parent.appendChild(li);
+		});
 	}
 
+	// document.getElementsByClassName('color__item')
+
+	let colors = ['Red', 'Green', 'Blue'];
+	let colorList = document.getElementById('colors');
+	let colorClass = 'color__item';
+	addItems(colors, colorList, colorClass);
+
+
+	let nums = ['one', 'two', 'three'];
+	let numberList = document.getElementById('numbers');
+	let numClass = 'number__item';
+	addItems(nums, numberList, numClass);
+
+
+
+
+
+
+
+
+
+
+
+
+	// let span = document.querySelector('span');
+	// let text = document.createTextNode(' Crane');
+	// span.appendChild(text);
+
+	// // span.innerText = ' Crane'
 
 })
 
--- week-4/d2-element-selection/lecture-demos/update-el-demo/your-code.js
+++ week-4/d2-element-selection/lecture-demos/update-el-demo/your-code.js
@@ -1,17 +1,109 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+	// innerHTML vs innerText
+
+	// let otherHeader = document.getElementById('other-header');
+	// otherHeader.innerText = '<h1>Woohoo</h1>';
+	// otherHeader.innerHTML = '<h1>Woohoo</h1>';
+
+
+
 	// TODO = CAPITALIZE H1 TEXT
 
+	// method #1
+
+	// let h1 = document.querySelector('h1'); // <h1>Hello World</h1>
+	// let oldText = h1.innerText; // 'Hello World'
+	// h1.innerText = oldText.toUpperCase();
+	// h1.innerText = h1.innerText.toUpperCase();
+
+
+	// method #2
+
+	let header = document.querySelector('header');
+	console.log(header);
+	header.innerHTML = '<h1 class="my-class">HELLO WORLD</h1>'
 
 
 
 	// TODO = CAPITALIZE LABELS
 
+	// method #1 nodelist
+
+	let labels = document.querySelectorAll('label');
+	labels.forEach(label => {
+		// label: <label>Color List</label>
+		let oldText = label.innerText; // 'Color List'
+		label.innerText = oldText.toUpperCase();
+	});
+
+
+	// method #2 htmlCollection
+
+	// let labels = document.getElementsByTagName('label');
+
+	// for (let i = 0; i < labels.length; i++) {
+	// 	let label = labels[i];
+	// 	label.innerText = label.innerText.toUpperCase();
+	// }
+
 	
+
 
 
 	// TODO = CAPITALIZE LIST ITEMS FOR COLORS AND NUMBERS
 
+
+	let listItems = document.querySelectorAll('li');
+	listItems.forEach(li => {
+		let oldText = li.innerText;
+		li.innerText = oldText.toUpperCase();
+	})
+
+
+
+
+
+
+
+
+
+	// HTML COLLECTION VS NODE LIST 
 	
+	// let htmlCollection = document.getElementsByClassName('animal__item');
+	// let nodeList = document.querySelectorAll('.animal__item');
+
+	// let listItems = document.querySelectorAll('li')
 	
+	// let animalList = document.getElementById('animals');
+	// // 	<ul id="animals">
+	// // 		<li class="animal__item">dogs</li>
+	// // 		<li class="animal__item">cats</li>
+	// // </ul>
+	// let newItem = document.createElement('li');
+	// // <li></li>
+	// newItem.innerText = 'pony';
+	// // <li>pony</li>
+	// newItem.setAttribute('class', 'animal__item');
+	// // <li class="animal__item">pony</li>
+	// animalList.appendChild(newItem);
+
+
+	// console.log('htmlCollection', htmlCollection);
+	// console.log('nodeList', nodeList);
+
+
+
+
+
+	// REMOVING ELEMENTS
+
+	// let animalList = document.getElementById('animals');
+	// animalList.remove()
+
+	// let firstItem = document.querySelector('.animal__item');
+	// animalList.removeChild(firstItem);
+	// firstItem.remove();
+
+
 })
