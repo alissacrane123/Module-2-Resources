d610a4e80f91dee11e1088e04dd32d8acf827f01
Alissa <alissacrane123@gmail.com>
2021-01-05 11:00:04 -0800

    update d2-element-selection lecture code

--- week-4/d2-element-selection/lecture-demos/select-el-demo/your-code.js
+++ week-4/d2-element-selection/lecture-demos/select-el-demo/your-code.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 	// 1. getElementById returns an element
-	let header = document.getElementById('my-header');
+	// let header = document.getElementById('my-header');
 	// console.log(header);
+
 
 	// 2. getElementByClassName returns an HTMLCollection
 	let colorItems = document.getElementsByClassName('color__item');
@@ -11,8 +12,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 	// 3. querySelectorAll returns a NodeList
-	// className (.className), id (#id), tagName(tagName)
+	//    - className (.className), id (#id), tagName(tagName)
 	let numberItems = document.querySelectorAll('.number__item');
+	// console.log(numberItems)
+
 	// let header = document.querySelectorAll('#my-header')
 	// let uls = document.querySelectorAll('ul');
 
@@ -25,15 +28,36 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// 5. querySelector returns an Element
 	let firstLabel = document.querySelector('label');
+	// console.log(firstLabel)
 
 
 
 	// TODO = ITERATE THROUGH NODE LIST AND CONSOLE.LOG EACH ITEM
 
+	// method #1 - using forEach
+	// numberItems.forEach(el => {
+	// 	console.log(el);
+	// });
+
+
+	// method #2 - for loop 
+	// for (let i = 0; i < numberItems.length; i++) {
+	// 	let el = numberItems[i];
+	// 	console.log(el);
+	// }
+
+
 
 
 	// TODO = ITERATE THROUGH HTML COLLECTION AND CONSOLE.LOG EACH ITEM
 
-	
-	
+	// error - cannot call forEach on an html collection
+	// colorItems.forEach(el => {
+	// 	console.log(el);
+	// });
+
+	for (let i = 0; i < colorItems.length; i++) {
+		let el = colorItems[i];
+		console.log(el);
+	}
 })
