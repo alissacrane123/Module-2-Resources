31c83b9f5cb804bb0b18beba04c613247e8ff159
Bart Dorsey <bart@bartdorsey.com>
2021-01-13 10:10:26 -0600

    Updating shelter demo to add static methods and public fields, and getter and setter demo

--- week-5/d2-classes/lecture-demos/complete-shelter-demo/dog.js
+++ week-5/d2-classes/lecture-demos/complete-shelter-demo/dog.js
@@ -21,6 +21,16 @@ class Dog extends Animal {
 			console.log(`${this.name} performs trick: ${trick}`)
 		})
 	}
+
+	static createDogs(dogPojos) {
+		return dogPojos.map(dog => {
+			return new Dog(dog.name, dog.age);
+		});
+	}
+
+	static everyonePerformsTricks(dogs) {
+		dogs.forEach(dog => dog.performTricks());
+	}
 }
 
 
